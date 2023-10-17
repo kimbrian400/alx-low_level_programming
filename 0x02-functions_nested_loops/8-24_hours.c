40ba85d22dd009a4edc159b1b42a688659053d06
kimbrian400 <kimbrian400@gmail.com>
2023-10-17 11:46:04 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function prints each minute of the day
+ * Description: it prints every minute of the day.
+ * return: no return
+*/
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
