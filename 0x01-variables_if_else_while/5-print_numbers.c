dcf090029ab75c83e5fb0a290446f4d7038ccb06
Brian Kimathi <kimbrian400@gmail.com>
2024-07-09 23:03:04 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
-#include <ctype.h>
 /**
- * main - Print all single digits of base 10 followed by new line
- *
- * Return: return 0
- *
- * FUNCTIONALITY *
- *
- * 1. Declare a variable called digit of type int.
- *
- * 2. Initialize the variable digit to the character ‘0’.
- *
- * 3. While the variable digit is less than or equal to the character ‘9’, do the following:
-	a. Print the value of the variable digit.
-	b. Increment the variable digit by 1.
-
- * 4. Print a newline character.
- *
- * 5. Return 0.
- *
- * Time Complexity: O(1)
+ * main - main block
+ * Description: Print all numbers of base 10, starting from 0.
+ * Return: 0
  */
 int main(void)
 {
-	int digit;
+	int a;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (a = 0; a < 10; a++)
 	{
-		putchar(digit);
+		printf("%i", a);
 	}
 	putchar('\n');
+
 	return (0);
 }
