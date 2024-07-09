201a2a5064d3842ac0755d7c831c3e764cdc13d6
Brian Kimathi <kimbrian400@gmail.com>
2024-07-09 23:10:11 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,34 @@
 #include <stdio.h>
 /**
- * main - printing combinations of two two digit numbers
- *
- * Return: Always 0 (success)
- *
- * FUNCTIONALITY *
- *
- * 1. It’s creating two variables, x and y, and initializing them to 0.
- *
- * 2. It’s creating a for loop that will run 100 times.
- *
- * 3. It’s creating a nested for loop that will run 100 times.
- *
- * 4. It’s checking if x is less than y.
- *
- * 5. If x is less than y, it’s printing the value of x and y.
- *
- * 6. It’s printing a comma and a space after each pair of numbers, except for the last pair.
- *
- * 7. It’s printing a new line after the last pair of numbers.
- *
- * Time Complexity: O(n^2)
+ * main - Prints multiple combinations
+ * Description: Write a program that prints all possible
+ * Return: 0
  */
 int main(void)
 {
-	int x, y;
+	int a, b, c, d;
 
-	for (x = 0; x < 100; x++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (y = 0; y < 100; y++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			if (x < y)
+			for (c = '0'; c <= '9'; c++)
 			{
-				putchar((x / 10) + 48);
-				putchar((x % 10) + 48);
-				putchar(' ');
-				putchar((y / 10) + 48);
-				putchar((y % 10) + 48);
-				if (x != 98 || y != 99)
+				for (d = '0'; d <= '9'; d++)
 				{
-					putchar(',');
-					putchar(' ');
+					if ((a < c) || (a == c && b < d))
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
+					if (!(a == '9' && b == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
 				}
 			}
 		}
